500f0a0264daa75660bd56b80d411029218386a4
Francisco Madariaga <fmg124@gmail.com>
2019-03-12 16:22:50 -0600

    Modificaciones para IOS

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts('js/sw-utils.js');
 
 
-const STATIC_CACHE    = 'static-v1';
+const STATIC_CACHE    = 'static-v3';
 const DYNAMIC_CACHE   = 'dinamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
